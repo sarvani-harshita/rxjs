50e3a867250c592491d49d9544d992adb92b1c72
sarvani-harshita <harshi.sarvani@gmail.com>
2021-09-08 21:20:51 +0530

    Create a new Custom Observable from scratch

---
+++ src/app/new-observable/new-observable.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NewObservableComponent } from './new-observable.component';
+
+describe('NewObservableComponent', () => {
+  let component: NewObservableComponent;
+  let fixture: ComponentFixture<NewObservableComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NewObservableComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NewObservableComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/new-observable/new-observable.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs'
+
+@Component({
+  selector: 'app-new-observable',
+  templateUrl: './new-observable.component.html',
+  styleUrls: ['./new-observable.component.css']
+})
+export class NewObservableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+   const newObservable$ = new Observable<Number>((observer)=>{
+    observer.next(1);
+    observer.next(2);
+    observer.next(3);
+    observer.next(4);
+    observer.complete();
+   })
+   let observer = {next: (data: number) => console.log(data), 
+  error: error =>console.log(error), complete: () =>console.log("completed")};
+
+  newObservable$.subscribe(observer)
+  }
+
+
+}
